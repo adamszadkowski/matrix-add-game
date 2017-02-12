7ae8c895999cb19218b180820cae92ed69906178
Adam Szadkowski <adam@szadkowski.info>
2017-02-12 18:58:51 +0100

    Create Game component

---
+++ matrix-add-game-rest-ui/src/main/resources/public/components/game/game.component.js
@@ -0,0 +1,14 @@
+(function (angular) {
+  'use strict';
+
+  angular
+    .module('game')
+    .component('game', {
+      templateUrl: 'components/game/game.template.html',
+      controller: function () {
+
+
+      }
+    });
+
+})(angular);
--- matrix-add-game-rest-ui/src/main/resources/public/components/game/game.module.js
+++ matrix-add-game-rest-ui/src/main/resources/public/components/game/game.module.js
@@ -1,5 +1,5 @@
 (function (angular) {
   'use strict';
 
-  angular.module('matrixAddGame', ['game']);
+  angular.module('game', ['gameGrid']);
 })(angular);
