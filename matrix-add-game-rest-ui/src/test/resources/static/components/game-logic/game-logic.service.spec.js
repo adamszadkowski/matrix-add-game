e686e2d431fbb9f04c78af473351639c6cb1afa6
Adam Szadkowski <adam@szadkowski.info>
2017-02-22 16:09:09 +0100

    Add GameLogic service

---
+++ matrix-add-game-rest-ui/src/test/resources/static/components/game-logic/game-logic.service.spec.js
@@ -0,0 +1,12 @@
+(function () {
+  'use strict';
+
+  describe('game logic', function() {
+    beforeEach(module('gameLogic'));
+
+    it('should execute', inject(function(GameLogic) {
+
+    }));
+
+  });
+})();
